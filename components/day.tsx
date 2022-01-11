6578cd208c72debc9482abad508f5d7232c1a259
scudmarx <darth_dunk@hotmail.com>
2022-01-11 10:39:23 +0000

    Style updates

--- components/day.tsx
+++ components/day.tsx
@@ -3,10 +3,12 @@ import ADL from './adl'
 
 const Day = ({date, record, handler}: {date: string, record: ICheckIn, handler: Function}): JSX.Element => {
     const timeline = parseInt(date.replace(/-/g,"")) - parseInt(toDayString(new Date()).replace(/-/g,""))
+    const datetime = new Date(parseInt(date.slice(0,4)), parseInt(date.slice(5,7))-1, parseInt(date.slice(8,10)))
+    const daytext = `${(["Sun","Mon","Tue","Wed","Thu","Fri","Sat"])[datetime.getDay()]} ${datetime.toLocaleDateString()}`
     var day_component = (
     <div key={date} className={`day ${timeline > 0 ? "future" : timeline < 0 ? "past" : "today"}`}>
         <div className={`dayhead`}>
-            {date}
+            {daytext}
         </div>
         {ADLs.map((adl, i) => <ADL 
             index={i}
