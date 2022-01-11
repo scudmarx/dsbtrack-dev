8fe2c826dbfcd29a61d5862f946cf297053ab38f
scudmarx <darth_dunk@hotmail.com>
2022-01-11 06:25:52 +0000

    Update day.tsx

--- components/day.tsx
+++ components/day.tsx
@@ -2,7 +2,7 @@ import { ADLs, ICheckIn, IRecordState, toDayString } from '../api/data'
 import ADL from './adl'
 
 const Day = ({date, record, handler}: {date: string, record: ICheckIn, handler: Function}): JSX.Element => {
-    const timeline = parseInt(date.replaceAll("-","")) - parseInt(toDayString(new Date()).replaceAll("-",""))
+    const timeline = parseInt(date.replace(/-/g,"")) - parseInt(toDayString(new Date()).replace(/-/g,""))
     var day_component = (
     <div key={date} className={`day ${timeline > 0 ? "future" : timeline < 0 ? "past" : "today"}`}>
         <div className={`dayhead`}>
