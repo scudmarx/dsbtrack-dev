30c259096b6c2e219e72bfc4d219d3b607797bb4
scudmarx <darth_dunk@hotmail.com>
2022-01-13 00:30:43 +0000

    Update adl.tsx

--- components/adl.tsx
+++ components/adl.tsx
@@ -5,7 +5,7 @@ const ADL = ({adl, achieved = false, handler = () => {}, index}: {adl: any, achi
         handler(!achieved)
     })
     return (
-    <div key={adl.key} className={`adl ${achieved ? "achieved" : ""} ${(["death", "shaft", "bombs"])[Math.floor(index / 5)]}`} onClick={handleClick}>
+    <div key={adl.key} title={adl.label} className={`adl ${achieved ? "achieved" : ""} ${(["death", "shaft", "bombs"])[Math.floor(index / 5)]}`} onClick={handleClick}>
         <img src={`/adl-icons${adl.icon}`} title={adl.label} className={`adl-icon`} />
         <span>{showlabels ? adl.label : ""}</span> <input type="checkbox" readOnly checked={achieved} />
     </div>)
