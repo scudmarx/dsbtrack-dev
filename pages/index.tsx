bfc99aa5bcfd2c34361a9c2fcbc2617742c62363
scudmarx <darth_dunk@hotmail.com>
2022-01-13 10:07:06 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,19 @@ import type { NextPage } from 'next'
 import { useRouter } from 'next/dist/client/router'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   const router = useRouter()
   const goTracker = () => {router.push("/tracker")}
-  
+  useEffect(() => {
+    (document.getElementsByClassName(styles.title)[0] as HTMLElement).style.transition = "visibility 0s 1s, opacity 1s linear"
+    setTimeout(() => {
+      (document.getElementsByClassName(styles.title)[0] as HTMLElement).style.visibility = "hidden";
+      (document.getElementsByClassName(styles.title)[0] as HTMLElement).style.opacity = "0";}, 1500);
+    setTimeout(goTracker, 3500)
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
@@ -18,7 +25,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title} onClick={goTracker}>
-          DSB Tracker
+          ADL Tracker
         </h1>
       </main>
 
