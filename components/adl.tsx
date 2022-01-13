539f711a1a28ce8f0f22954a9d6e07a424a934a0
scudmarx <darth_dunk@hotmail.com>
2022-01-13 08:47:09 +0000

    Update adl.tsx

--- components/adl.tsx
+++ components/adl.tsx
@@ -1,4 +1,5 @@
 import { IConfigState } from "../api/data"
+import Image from "next/image"
 
 const ADL = ({adl, achieved = false, handler = () => {}, index, config}: {adl: any, achieved: boolean, handler: Function, config: IConfigState, index: number}): JSX.Element => {
     const showlabels = false
@@ -7,7 +8,7 @@ const ADL = ({adl, achieved = false, handler = () => {}, index, config}: {adl: a
     })
     return (
     <div key={adl.key} title={adl.label} className={`adl ${achieved ? "achieved" : ""} ${adl.group}`} onClick={handleClick}>
-        <img src={`/adl-icons/${adl.icon}`} title={adl.label} className={`adl-icon`} />
+        <img alt={adl.label} src={`/adl-icons/${adl.icon}`} title={adl.label} className={`adl-icon`} />
         <span>{showlabels ? adl.label : ""}</span> <input type="checkbox" readOnly checked={achieved} />
     </div>)
 }
